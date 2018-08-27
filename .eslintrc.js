7bd6f145dc52596d792ffb27a057dbea8029ffb2
zkl2333 <i@zkl2333.com>
2018-08-27 10:50:00 +0800

    配置开发环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-    root: true, 
-    extends: 'eslint:recommended',
-    "env":{
-        "node":true,
-      },
-    "parserOptions": {
-        "ecmaVersion": 8
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "semi": ["warn", "never"],
-        "quotes": "off",
-        "no-console":"off",
-        "no-unused-vars":"off",
-        "no-unreachable":"off",
-        "no-redeclare":"warn"
-    }
-};
+	root: true,
+	extends: 'eslint:recommended',
+	env: {
+		node: true
+	},
+	parserOptions: {
+		ecmaVersion: 8
+	},
+	rules: {
+		indent: ['warn', 'tab'],
+		semi: ['warn', 'never'],
+		quotes: 'off',
+		'no-console': 'off',
+		'no-unused-vars': 'off',
+		'no-unreachable': 'off',
+		'no-redeclare': 'warn'
+	}
+}
