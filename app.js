bc8ae84c9b2c5cbf965cd806b70f8cb094286fe5
zkl2333 <i@zkl2333.com>
2018-08-27 11:18:09 +0800

    返回自增id

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const wss = new WebSocket.Server({
 	server
 })
 
+var runtimes = 7
 wss.on('connection', function connection(ws) {
 	ws.on('message', function incoming(message) {
 		// console.log('received: %s', message)
@@ -13,34 +14,42 @@ wss.on('connection', function connection(ws) {
 			let allTtems = {
 				Item: [
 					{
+						id: 0,
 						name: '张三',
 						hear_img: './img/1.jpg'
 					},
 					{
+						id: 1,
 						name: '222',
-						hear_img: './img/2.jpg'
+						hear_img: './img/2.jpeg'
 					},
 					{
+						id: 2,
 						name: '333',
 						hear_img: './img/3.jpeg'
 					},
 					{
+						id: 3,
 						name: '444',
 						hear_img: './img/4.jpg'
 					},
 					{
+						id: 4,
 						name: '555',
 						hear_img: './img/5.jpg'
 					},
 					{
+						id: 5,
 						name: '666',
 						hear_img: './img/6.jpg'
 					},
 					{
+						id: 6,
 						name: '777',
 						hear_img: './img/7.jpg'
 					},
 					{
+						id: 7,
 						name: '888',
 						hear_img: './img/8.png'
 					}
@@ -49,11 +58,11 @@ wss.on('connection', function connection(ws) {
 			ws.send(JSON.stringify(allTtems))
 		} else if (message === 'HeartBeat') {
 			ws.send('HeartBeat')
-			var runtimes = 0
 			runtimes++
 			ws.send(
 				JSON.stringify({
 					add: {
+						id: runtimes,
 						name: 'runtimes' + runtimes,
 						hear_img: './img/8.png'
 					}
